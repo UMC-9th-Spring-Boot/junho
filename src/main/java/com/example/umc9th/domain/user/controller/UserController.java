259c36a67ce71dfd43b7bd8666f03741934a7f56
김준호 <2171168@hansung.ac.kr>
2025-11-01 15:33:14 +0900

    faet: 응답 통일, 기존 api 리팩토링

--- src/main/java/com/example/umc9th/domain/user/controller/UserController.java
+++ src/main/java/com/example/umc9th/domain/user/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.umc9th.domain.user.controller;
 
 import com.example.umc9th.domain.user.service.UserService;
+import com.example.umc9th.global.apiPayload.ApiResponse;
+import com.example.umc9th.global.apiPayload.code.BaseSuccessCode;
+import com.example.umc9th.global.apiPayload.code.status.GeneralSuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +16,7 @@ public class UserController {
     private final UserService userService;
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<Void> deleteUser(@PathVariable("userId") Long userId) {
-        userService.deleteUser(userId);
-        return ResponseEntity.noContent().build();
+    public ApiResponse<String> deleteUser(@PathVariable("userId") Long userId) {
+        return ApiResponse.onSuccess(GeneralSuccessCode._DELETED, userService.deleteUser(userId));
     }
 }
